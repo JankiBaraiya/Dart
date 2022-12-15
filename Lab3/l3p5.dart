9f8af4cf87cf7f3d2990dfeaedf2a82355bdd3af
JankiBaraiya <108472423+JankiBaraiya@users.noreply.github.com>
2022-12-15 18:18:04 +0530

    Update l3p5.dart

--- Lab3/l3p5.dart
+++ Lab3/l3p5.dart
@@ -1,14 +1,16 @@
 
+
 import 'dart:io';
 
 void main(List<String> args) {
   print("Enter String");
   String s = stdin.readLineSync()!;
-  // List<String> list= ;
 
   // for(int i=s.length-1;i>=0;i--)
   // {
-    print("${s.split('').reversed.toString()}");
+  //   print("${s[i]}");
   // }
+
+  print("${s.split('').reversed.toString()}");
   
 }
