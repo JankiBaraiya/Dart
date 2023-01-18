a12d313715986afedf840c39d930d893252b2b94
JankiBaraiya <108472423+JankiBaraiya@users.noreply.github.com>
2023-01-18 18:19:39 +0530

    Update l2p6.dart

--- Lab2/l2p6.dart
+++ Lab2/l2p6.dart
@@ -6,5 +6,35 @@ void main(List<String> args) {
   print("Enter Number2");
   int n2 = int.parse(stdin.readLineSync()!);
 
+  print("Enter Choice \n 1 for Add \n 2 for Sub \n 3 for Multi \n 4 for Divi ");
+  int ch = int.parse(stdin.readLineSync()!);
+
+  int ans;
+
+  switch(ch)
+  {
+    case 1:
+      ans=n1+n2;
+      print("Ans=$ans");
+      break;
+
+    case 2:
+      ans=n1-n2;
+      print("Ans=$ans");
+      break;
+
+    case 3:
+      ans=n1*n2;
+      print("Ans=$ans");
+      break;
+
+    case 4:
+      ans=(n1/n2).toInt();
+      print("Ans=$ans");
+      break;
+
+    default :
+      print("Enter valid Choice");
+  }
   
 }
